306498d0805266cefc2c4718d0a7d55daa42d6ab
Husayn <hussainiahmed222@gmail.com>
2023-04-25 18:49:12 +0100

    Add files via upload

---
+++ Hello_world.py
@@ -0,0 +1,25 @@
+#String
+print("Hello world")
+
+#integer
+print(574)
+
+#float
+print(0.65)
+
+#boolean
+print(True)
+print(False)
+
+#String practice
+your_Name = input("What is your name\n")
+first_Char = your_Name[0]
+print("hello " + your_Name + ", your name starts with a letter " + first_Char)
+
+#Type casting
+my_Num = input("Enter a two digit number\n")
+first_Num = str(my_Num[0])
+second_Num = str(my_Num[1]);
+
+print(first_Num)
+print(second_Num)
